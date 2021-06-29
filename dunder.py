3984ce35a43a2d1e14a00964bfd9a7e08084a55f
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-06-29 20:00:15 -0300

    :tada: estudos 29/06

---
+++ dunder.py
@@ -0,0 +1,14 @@
+"""
+Dunder Name e Dunder Main (double under)
+
+dunder name -> __name__
+dunder main -> __main__
+
+Em Python, são utilizados dunder para criar funções, atributos, propriedades e
+etc utilizando Double Under para não gerar conflito com os nomes desses elementos
+na programação.
+"""
+
+from modulo_dunder import soma
+
+print(soma(1, 1))
---
+++ modulo_dunder.py
@@ -0,0 +1,11 @@
+
+
+def soma(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    num1, num2 = 0, 0
+    print(soma(num1, num2))
+else:
+    print('O módulo modulo_dunder foi importado!')
