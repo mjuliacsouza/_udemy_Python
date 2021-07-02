d79e4e2a4426e7bec1298257495ccd2d83dc0f23
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-02 12:17:30 -0300

    :tada: estudos 02/07

---
+++ secao_13/9_sistemas_arquivos_manipulacao.py
@@ -0,0 +1,35 @@
+"""
+Sistema de Arquivos - Manipulação
+
+import os
+
+# Descobrindo se um arquivo ou diretório existe
+print(os.path.exists('./teste/oi.txt'))  # True
+print(os.path.exists('../secao_13'))  # True
+
+# Criando arquivos
+
+# Forma 1
+open('./teste/TESTE.txt', 'w').close()
+open('./teste/TESTE2.txt', 'a').close()
+
+# Forma 2
+# os.mknod('./teste/TESTE3.txt'), não funciona no windows
+
+# Criando diretórios
+os.mkdir('teste2')  # wtf, esse funciona
+
+# OBS: Se não tivermos autorização para usar o diretório, teremos PermissionError
+# Podemos ignorar com o parâmetro exist_ok=True
+
+# Renomeando
+os.rename('teste2', 'teste02')
+
+# Podemos ter muitos erros
+
+# Removendo diretórios vazios
+os.rmdir('teste02')
+"""
+
+import os
+
