baf539b39986f040811762cf5c029a2efb91d94e
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-03 14:41:07 -0300

    :tada: estudos 03/07

---
+++ secao_14/3_criando_loops.py
@@ -0,0 +1,27 @@
+"""
+Criando sua própria versão de loop
+
+
+for num in [1, 2, 3, 4, 5]:
+    print(num)
+
+for letra in 'Geek University':
+    print(letra)
+
+iter([1, 2, 3, 4, 5])
+iter('Geek University')
+"""
+
+# O que acontece num for por debaixo dos panos:
+
+
+def meu_for(iteravel):
+    it = iter(iteravel)
+    while True:
+        try:
+            print(next(it))
+        except StopIteration:
+            break
+
+
+meu_for('Geek University')
