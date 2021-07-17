b70f9ff8a22382ea98bde60f7bd0ce378b5ce8c1
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-17 12:13:54 -0300

    :tada: estudos 17/07

---
+++ secao_16/3_classes.py
@@ -0,0 +1,29 @@
+"""
+POO - Classes
+
+Classes, nada mais são, do que modelos dos objetos do mundo real sendo
+representados computacionalmente.
+
+Classes podem conter:
+    - Atributos => Representam as características do objeto
+
+    - Métodos (funções) => Representam os comportamentos do objeto, as
+    ações que podem realizar no sistema
+
+Em Python, para definir uma classe utilizamos uma palavra reservada 'class'.
+
+Quando nomeamos uma classe em Python, utilizamos por convenção uma inicial em maiucula.
+"""
+
+
+class Lampada:
+    pass  # para ignorar
+
+
+# ao criar a classe, cria-se um tipo de dado
+lamp = Lampada()
+print(type(lamp))
+
+# os inteiros também são uma classe (interna do Python)
+numero = int('42')
+print(type(numero))
