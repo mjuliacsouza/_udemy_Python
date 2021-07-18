a86c92b924c5966665d1ee328c13bc4bb23509a1
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-18 13:44:10 -0300

    :tada: estudos 18/07

---
+++ secao_17/3_propriedades.py
@@ -0,0 +1,34 @@
+"""
+POO - Propriedades (Properties)
+"""
+
+
+class Conta:
+
+    contador = 0
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta.contador + 1
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        return f'Saldo de {self.__saldo} do cliente {self.__titular}'
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.__saldo -= valor
+        destino.__saldo += valor
+
+
+conta1 = Conta('Nicolas', 2000, 10000)
+conta2 = Conta('Julia', 5000, 50000)
+
+print(conta1.extrato())
+print(conta2.extrato())
