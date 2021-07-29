50a138f1e0c5563dde46c34286b1b6c7679b8128
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-29 07:54:26 -0300

    :tada: estudos 27/07

--- secao_18/3_escrevendo_csv.py
+++ secao_18/3_escrevendo_csv.py
@@ -1,3 +1,38 @@
 """
 Escrevendo em arquivos CSV
+
+from csv import writer
+
+with open('filmes.csv', 'w') as arquivo:
+    escritor_csv = writer(arquivo)
+    filme = None
+    escritor_csv.writerow(['Título', 'Gênero', 'Duração'])
+    while filme != 'sair':
+        filme = input('Informe o nome do filme: ')
+        if filme != 'sair':
+            genero = input('Informe o gênero: ')
+            duracao = input('Informe a duração em minutos: ')
+            escritor_csv.writerow([filme, genero, duracao])
+
+
+# se o arquivo já existir, ele é sobreescrito
 """
+
+# DictWriter
+
+from csv import DictWriter
+
+with open('filmes2.csv', 'w') as arquivo:
+    cabecalho = ['Título', 'Gênero', 'Duração']
+    escritor_csv = DictWriter(arquivo, fieldnames=cabecalho)
+    escritor_csv.writeheader()
+    filme = None
+    while filme != 'sair':
+        filme = input('Informe o nome do filme: ')
+        if filme != 'sair':
+            genero = input('Informe o gênero do filme: ')
+            duracao = input('Informe a duração do filme: ')
+            escritor_csv.writerow({'Título': filme, 'Gênero': genero, 'Duração': duracao})
+
+
+# as chaves tem que equivaler ao cabeçalho
