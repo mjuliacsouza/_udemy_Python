10650c20a8b47c4ba630b067791cd0c78f0da27c
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-30 21:16:51 -0300

    :tada: estudos 27/07

--- secao_19/4_metodos_datas_horas.py
+++ secao_19/4_metodos_datas_horas.py
@@ -35,7 +35,6 @@ print(manutencao.weekday())  # em números
 # 5: sábado
 # 6: domingo
 
-
 aniversario = input('Informe sua data de nascimento (dd/mm/yyyy): ')
 aniversario = aniversario.split('/')
 aniversario = datetime.datetime(year=int(aniversario[2]), month=int(aniversario[1]), day=int(aniversario[0]))
@@ -51,9 +50,8 @@ dias_da_semana = {'segunda-feira': 0,
 for key, value in dias_da_semana.items():
     if aniversario.weekday() == value:
         print(f'Você nasceu em {key}')
-"""
 
-import datetime
+from textblob import TextBlob
 
 # formatando
 
@@ -63,3 +61,36 @@ print(hoje)
 
 hoje_formatado = hoje.strftime('%d/%m/%Y')  # se usar %y fica apenas dois numero
 print(hoje_formatado)
+
+hoje_formatado = hoje.strftime('%B %d, %Y')  # nome do mês em ingles
+print(hoje_formatado)
+
+print(TextBlob(hoje.strftime('%B')).translate(to='pt-br'))  # traduz para pt
+
+# Transformando string em datetime
+
+nascimento = datetime.datetime.strptime('07/11/1997', '%d/%m/%Y')
+print(nascimento)
+print(type(nascimento))
+print(repr(nascimento))
+
+# Somente a hora
+almoco = datetime.time(12, 30, 0)
+print(almoco)
+"""
+
+import timeit
+
+# Tempo de execuçao
+
+# Loop for
+tempo = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
+print(tempo)  # 0.2858718
+
+#List comprehension
+tempo = timeit.timeit('"-".join([str(n) for n in range(100)])', number=10000)
+print(tempo)  # 0.22382919999999995
+
+# Map
+tempo = timeit.timeit('"-".join(map(str, range(100)))', number=10000)
+print(tempo)  # 0.19083889999999992
