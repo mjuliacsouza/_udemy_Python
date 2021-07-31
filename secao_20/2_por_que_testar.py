f0b9b2ca7b45118bde8bd01a9903b209fd983db4
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-07-31 20:29:45 -0300

    :tada: estudos 27/07

---
+++ secao_20/2_por_que_testar.py
@@ -0,0 +1,21 @@
+"""
+Por que testar o código?
+"""
+
+
+class Gato:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    def miar(self):
+        print(f'{self.__nome} está miando...')
+
+
+felix = Gato('Felix')
+felix.miar()
+print(felix.nome)
