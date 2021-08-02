cd58d5f662b017fb6f71ee3acf84f022e2513ae2
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-08-02 15:49:51 -0300

    :tada: estudos 02/08

--- secao_20/4_doctests.py
+++ secao_20/4_doctests.py
@@ -1,3 +1,17 @@
 """
 DocTests
+
+São testes que colocamos em docstrings das funções/métodos Python.
 """
+
+
+def soma(a, b):
+    """soma os número a e b
+
+    >>> soma(1, 2)
+    3
+    """
+    return a + b
+
+
+# print(soma(2, 4))  # 6
