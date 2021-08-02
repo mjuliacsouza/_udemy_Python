360d399fb0e702f7983f6fb4283152329c7dea6c
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-08-02 16:06:46 -0300

    :tada: aulas 02/08

--- secao_20/4_doctests.py
+++ secao_20/4_doctests.py
@@ -15,3 +15,40 @@ def soma(a, b):
 
 
 # print(soma(2, 4))  # 6
+
+# Aplicando o TDD
+
+
+def duplicar(valores):
+    """duplica os valores em uma lista
+
+    >>> duplicar([1, 2, 3, 4])
+    [2, 4, 6, 8]
+
+    >>> duplicar([])
+    []
+
+    >>> duplicar(['a', 'b', 'c', 'd'])
+    ['aa', 'bb', 'cc', 'dd']
+
+    >>> duplicar([True, None])
+    Traceback (most recent call last):
+        ...
+    TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
+    """
+    lista = [2 * elemento for elemento in valores]
+    return lista
+
+
+print(duplicar([1, 1, 1]))
+
+# Erro inesperado
+
+
+def fala_oi():
+    """fala oi
+
+    >>> fala_oi()
+    'oi'  # colocar aspas simples aqui dentro
+    """
+    return 'oi'
