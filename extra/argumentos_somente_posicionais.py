5219711dee52f14c59f5fab71a15a29ec5a395dc
Maria Júlia Cristofoletti de Souza <mjuliacsouza@gmail.com>
2021-08-09 19:49:28 -0300

    :tada: estudos 09/08

--- extra/argumentos_somente_posicionais.py
+++ extra/argumentos_somente_posicionais.py
@@ -1,3 +1,23 @@
 """
 Argumentos somente posicionais
 """
+
+valor = "67.3"
+print(float(valor))
+# print(floar(x=valor)) # não funciona
+
+
+def cumprimenta_v1(nome):
+    return f'Olá {nome}!'
+
+
+print(cumprimenta_v1('Julia'))
+print(cumprimenta_v1(nome='Julia'))
+
+
+def cumprimenta_v2(nome, /):
+    return f'Olá {nome}!'
+
+
+print(cumprimenta_v2('Julia'))
+# print(cumprimenta_v2(nome='Julia'))  # erro, argumento apenas posicional
